67e6c25d6c4a255f7b6e391a0736305d338916db
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2023-11-30 13:38:25 +0300

    alex_cars_2

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('.header__burger').addEventListener('click', function() {
+        this.classList.toggle('active');
+        document.querySelector('.header__menu').classList.toggle('active');
+        document.querySelector('body').classList.toggle('lock');
+    });
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect:'cube',
+    freeMode:true,
+    slidesPerView: 3,
+    centeredSlides:true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
